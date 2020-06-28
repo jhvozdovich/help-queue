f701b70a2a21c02c799c0533017ff85379ce3b4c
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-06-28 11:26:11 -0700

    refactor with JSX loop and ticket object array

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -1,17 +1,33 @@
 import React from "react";
 import Ticket from "./Ticket";
 
+const masterTicketList = [
+  {
+    names: "Jessica and Nick",
+    location: "3a",
+    issue: "Firebase will not save record!"
+  },
+  {
+    names: "Josh and Chelsea",
+    location: "11a",
+    issue: "Pupper has eaten the hardware!"
+  },
+  {
+    names: 'Imani & Jacob',
+    location: '9F',
+    issue: 'Child component isn\'t rendering.'
+  }
+];
 function TicketList() {
   return (
     <React.Fragment>
-      <Ticket
-        location="3a"
-        names="Jessica and Nick"
-        issue="Firebase will not save record!" />
-      <Ticket
-        location="11a"
-        names="Josh and Chelsea"
-        issue="Pupper has eaten the hardware!" />
+      <hr />
+      {masterTicketList.map((ticket, index) =>
+        <Ticket names={ticket.names}
+          location={ticket.location}
+          issue={ticket.issue}
+          key={index} />
+      )}
     </React.Fragment>
   );
 }
